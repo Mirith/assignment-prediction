523d07bbc71a462c58bf64bb3edc5f97b88753e8
Lauren Shin <laurenshin15@gmail.com>
2017-12-03 21:11:06 -0800

    made workhours simpler

--- assignments analysis.R
+++ assignments analysis.R
@@ -21,9 +21,10 @@
 # fix headings -- all heading1...
 # explore workhours -- verbose... look at summary only? 
 # cut some of the exploration that is simple, look at summary?
-# explore data after dropping stuff
+# explore data after dropping stuff visually
 
 rm(list=ls(all=TRUE)) # clear memory
+
 #' setting the working directory, ie where data is  
 #' works for local machine, will have to change for others' though
 setwd("~/LING 104/assignments 1") 
@@ -35,7 +36,26 @@ summary(x <- read.delim("assignments.csv"))
 # to make things easier to see in plots
 colors <- c("blue", "dark green", "red")
 
-barplot(table(x$MISTAKES)) # looks like a pretty normal distribution of mistakes
+sort(x$WORKHOURS)
+
+workhours.simple <- c()
+
+for(num in 12:39)
+{
+    workhours.simple <- append(workhours.simple, length(which(x$WORKHOURS >= num & x$WORKHOURS < num+1)))
+}
+
+workhours.simple
+
+names <- c(12:39)
+
+barplot(workhours.simple, 
+        ylab = "people", 
+        xlab = "hours worked", 
+        names.arg = names, 
+        col = "dark blue")
+
+barplot(table(x$MISTAKES), col = "dark blue") # looks like a pretty normal distribution of mistakes
 
 barplot(table(x$SEX), col = colors) # about half and half
 
@@ -55,10 +75,10 @@ barplot(table(x$ASSIGNMENT, x$SEX),
         col = colors,
         ylab = "number") 
 
+#' ##Region v Assignment
 table(x$ASSIGNMENT, x$REGION)
 # sort of unclear, lots of things going on
 
-#' ##Region v Assignment
 barplot(prop.table(table(x$REGION, x$ASSIGNMENT)[1,]), 
         legend.text = TRUE, 
         col = colors, 
@@ -75,6 +95,7 @@ barplot(prop.table(table(x$REGION, x$ASSIGNMENT)[3,]),
         main = "Middle Eastern",
         ylab = "percentage") 
 
+# GIVE NAME
 # each group has a spike in numbers in a separate format
 # ie, oral exam for CE, thesis for H, and lab report for ME
 # easy to see the large spike in plot, all together
@@ -97,10 +118,17 @@ barplot(table(x$ASSIGNMENT, x$MISTAKES),
 # getting the data as vectors
 attach(x)
 
+table(x$WORKHOURS) # is really messy, cleaning it up a bit
+
+for (num in c(1:40))
+{
+    print(sum(x$WORKHOURS[num:num+1]))
+}
+
 #' #Creating the regression model
 # using a glm as the dependent variable is categorical
 summary(model.01 <- glm(ASSIGNMENT ~ (SEX+REGION+WORKHOURS+MISTAKES)^3, 
-        data = x[complete.cases(x),], 
+        data = x, 
         family = binomial))
 
 #' #Testing for significance
@@ -133,7 +161,37 @@ summary(model.06 <- update(model.05, ~. - SEX:MISTAKES))
 
 drop1(model.06, test="Chisq")
 
+#summary(model.07 <- update(model.06, ~. - REGION:MISTAKES))
+
+#drop1(model.07, test="Chisq")
+
 #' Everything is now significant  
 
-#' #Exploring the model visually
-# asdk;flsdfflkj
+#' #Exploring the model 
+
+# significance test for model
+# comparing it to a null model, ie one with no predictors
+# p-value of 2.2e-16, ie significant
+anova(model.06,
+      glm(ASSIGNMENT ~ 1,
+          data=x,
+          family=binomial),
+      test="Chisq") 
+
+rms::lrm(formula(model.06))$stats[c("C", "R2")]
+# r squared with mistakes is 0.5776149
+# model.07 is only 0.5647909 -- that's pretty low
+# range is .5 to 1...
+
+
+
+# sample summary
+
+# summarizing everything #######################################################
+# model.07 is significant (p-value of 2.2e-16) but has a low R-squared (about .565)
+# value, indicating that the connection between the assignment chosen and 
+# a number of independent variables is significant but noisy. 
+# model.06 includes interactions between
+# SEXmale:REGIONhispanic, SEXmale:REGIONmiddle eastern, SEXmale:WORKHOURS, 
+# REGIONhispanic:WORKHOURS, REGIONmiddle eastern:WORKHOURS, WORKHOURS:MISTAKES  
+# and SEXmale, WORKHOURS, MISTAKES, REGIONhispanic, REGIONmiddle eastern
